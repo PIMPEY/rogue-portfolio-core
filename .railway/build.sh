e8981ff21ecacedaf43cfad65a982bfb93651d71
PIMPEY <PJ@dariuscubed.com>
2025-12-30 10:39:18 +0000

    $(cat <<'EOF'

--- .railway/build.sh
+++ .railway/build.sh
@@ -11,10 +11,6 @@ cd app-web
 echo "Generating Prisma Client..."
 npx prisma generate
 
-# Run migrations
-echo "Running database migrations..."
-npx prisma migrate deploy
-
 # Build Next.js app
 echo "Building Next.js app..."
 npm run build
